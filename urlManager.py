4b18086be939e03d6a2d850d2027ab755c2882e8
Administrator <124695494@qq.com>
2017-02-17 10:04:29 +0800

    "first commit"

--- urlManager.py
+++ urlManager.py
@@ -1,3 +1,48 @@
 #!/usr/bin/env python
 # -- coding:utf-8 --
 
+import re,sys
+
+def Add():
+    pass
+
+def Modify():
+    pass
+
+def Delete():
+    pass
+
+def Find():
+    pass
+
+def Show()
+    pass
+
+def showMenu():
+    prompt = """
+    (A)dd url
+    (M)odify url
+    (D)elete url
+    (F)ind url
+    (S)how url
+    (Q)uit
+    Enter Choice:"""
+    while True:
+        try:
+            choice = raw_input("your choice:").strip().lower()
+        except ValueError,KeyboardInterrupt:
+            choice = "q"
+        if choice not in "amdfsq":
+            choice = "q"
+        elif choice == "a":
+            Add()
+        elif choice == "m":
+            Modify()
+        elif choice == "d":
+            Delete()
+        elif choice == "f":
+            Find()
+        elif choice == "s":
+            Show()
+        elif choice == "q"
+            break
