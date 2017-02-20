8ee85c14b40d7998a8c9053f3522da898394c9a8
Administrator <124695494@qq.com>
2017-02-20 19:58:26 +0800

    "函数执行时间花费测试"

--- timeit.py
+++ timeit.py
@@ -9,11 +9,14 @@ def func(x):
         time.sleep(1)
 
 #函数时间性能测试
-def testit(func1,x):
+def testit(func1,x=5):
     t1 = time.clock()
     func1(x)
     t2 = time.clock()
     return(t2-t1)
 
 timecost = testit(func,10)
+print("time cost:%s" %(timecost))
+
+timecost = testit(func)
 print("time cost:%s" %(timecost))
