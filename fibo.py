63f9388001afedadab5acb035d0ef7626779063a
Administrator <124695494@qq.com>
2017-02-22 09:55:08 +0800

    "time complex"

--- fibo.py
+++ fibo.py
@@ -3,7 +3,7 @@
 
 import time
 
-#返回
+#x相当于控制递归层数，当x大于某个层级如40时，执行存在问题
 def fibo1(x):
     if x == 0 or x == 1:
         return 1
@@ -19,6 +19,15 @@ def fibo2(x):
             fibo_list.append(fibo_list[i-1]+fibo_list[i-2])
     return(fibo_list)
 
-print(fibo1(5))
-print(fibo2(5))
+#时间复杂度分析
+if __name__ == "__main__":
+    t1 = time.clock()
+    result1 = fibo1(40)
+    t2 = time.clock()
+    t3 = time.clock()
+    result2 = fibo2(10000)
+    t4 = time.clock()
+    print("fibo1(40) cost time:%s" %(t2-t1))
+    print("fibo2(10000) cost time:%s" %(t4-t3))
+
 
