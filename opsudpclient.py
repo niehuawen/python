6f87db8a925b77a89e203f5e0ae6415b338078ef
Administrator <124695494@qq.com>
2017-04-07 16:06:43 +0800

    tcp_server

---
+++ opsudpclient.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+#--coding:utf-8--
+
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+for data in ["nie","hua","wen"]:
+    #发送数据
+    s.sendto(data,("127.0.0.1",9999))
+    #接收数据
+    print(s.recv(1024))
+s.close()
+
