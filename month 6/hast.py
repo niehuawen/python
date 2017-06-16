9d34b3e57f0dd052c1238882101efd6b6a420db3
lenovo <124695494@qq.com>
2017-06-16 23:12:25 +0800

    hash()

---
+++ month 6/hast.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -- coding:utf-8 --
+# description: hash() 返回整数
+
+a = 1
+b = 1.0
+c = "abc"
+
+a_hash = hash(a)
+b_hash = hash(b)
+c_hash = hash(c)
+
+print((a_hash,b_hash,c_hash))
+
