8415bbb4cd1938bc52b611c7386dc898697f4342
lenovo <124695494@qq.com>
2017-07-09 11:46:49 +0800

    二元运算

--- month 6/chapter5/5-6.py
+++ month 6/chapter5/5-6.py
@@ -24,12 +24,32 @@ def cal(num1,ops,num2):
 
 def main():
     while True:
-        number1 = enter()
-        op = raw_input("please input a ops (+-*/%**):").strip()
-        if op not in ("+","-","*","/","%","**"):
-            print("op input error,please enter again.")
-            continue
-        number2 = enter()
+        # 方式一
+        # number1 = enter()
+        # op = raw_input("please input a ops (+-*/%**):").strip()
+        # if op not in ("+","-","*","/","%","**"):
+        #     print("op input error,please enter again.")
+        #     continue
+        # number2 = enter()
+
+        #方式二
+        info = raw_input("please input your express:").strip()
+        infolist = info.split()
+        try:
+            n1 = infolist[0].strip()
+            n2 = infolist[2].strip()
+            if "." in n1:
+                number1 = float(n1)
+            else:
+                number1 = int(n1)
+            if "." in n2:
+                number2 = float(n2)
+            else:
+                number2 = int(n2)
+        except Exception as e:
+            print(e)
+            break
+        op = infolist[1].strip()
         result = cal(number1,op,number2)
         print("%s %s %s = %s" % (number1,op,number2,result))
 
